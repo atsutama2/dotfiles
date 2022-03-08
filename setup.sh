26b60af15007e4a19bf440f7d826e7656c65d2e2
atsutama <atsutama.0713@gmail.com>
2022-03-08 15:27:40 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,8 +12,8 @@ do
 done
 
 # git設定
-git config --local user.name "atsutama2"
-git config --local user.email "atsutama.0713@gmail.com"
+# git config --local user.name "atsutama2"
+# git config --local user.email "atsutama.0713@gmail.com"
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
